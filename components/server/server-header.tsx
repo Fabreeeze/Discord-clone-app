b5c249acada8340853cac5eb69de6f0fc1064bf1
akhram.ark@gmail.com <97621761+Fabreeeze@users.noreply.github.com>
2024-06-17 07:31:25 +0530

    commit 18, added "create channel" functionality

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -81,6 +81,7 @@ export const ServerHeader = ({
                     <DropdownMenuItem
                         className="px-3 py-2 text-sm
                         cursor-pointer"
+                        onClick = { () => onOpen("createChannel")}
                     >
                         Create Channel
                         <PlusCircle className="h-4 w-4 ml-auto" />
