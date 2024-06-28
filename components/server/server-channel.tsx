4aa4387d28e3c40ef0c915cab182f60058cd856c
akhram.ark@gmail.com <97621761+Fabreeeze@users.noreply.github.com>
2024-06-29 04:41:24 +0530

    commit 26, open 'general' channel on server open and onclick functions to channel name and member name added

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -5,7 +5,7 @@ import { Channel, ChannelType, MemberRole, Server } from "@prisma/client";
 import { Hash, Mic, Video,Edit,Trash,Lock } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import { ActionTooltip } from "@/components/action-tooltips";
-import { useModal } from "@/hooks/use-modal-store";
+import { useModal , ModalType } from "@/hooks/use-modal-store";
 
 interface ServerChannelProps{
     channel: Channel;
@@ -31,12 +31,25 @@ export const ServerChannel = ({
     const { onOpen } = useModal();
 
     const params = useParams();
-    const rounter = useRouter();
+    const router = useRouter();
 
     const Icon  = iconMap[channel.type];
 
+    const onClick = () => {
+        router.push(`/servers/${params?.serverId}/channels/${channel.id}`);
+    }
+
+    const onAction = (e : React.MouseEvent, action : ModalType) => {
+        e.stopPropagation();
+        onOpen(action , {channel , server })
+    }
+    // we add this onAction so as to differentiate the clicks on the edit or 
+    // delete buttons vs the clicks on the channel name to open the channel as
+    //  botht he buttons(edit and delete ) occupy the same area space on screen 
+    // as the channel name where clicking triggers opening the channel page evetn
+
     return ( 
-        <button onClick={() => {}}
+        <button onClick={onClick}
             className = {cn("group px-2 py-2 rounded-md items-center flex gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinz-700/50 transition mb-1",
                 params?.channelId === channel.id && " bg-zinc-700/20 dark:text-zinc-700"
             )}
@@ -50,13 +63,13 @@ export const ServerChannel = ({
             { channel.name !== "general" && role !== MemberRole.GUEST && (
                 <div className="ml-auto flex items-center gap-x-2">
                     <ActionTooltip label="Edit">
-                        <Edit onClick={ () => onOpen("editChannel",{server , channel})} 
+                        <Edit onClick={ (e) => onAction(e,"editChannel")} 
                             className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc300 transition"
                          />
                     </ActionTooltip>
                     <ActionTooltip label="Delete">
                         <Trash 
-                        onClick={ () => onOpen("deleteChannel",{server,channel})} 
+                        onClick={ (e) => onAction(e,"deleteChannel") } 
                         className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc300 transition"
                          />
                     </ActionTooltip>
